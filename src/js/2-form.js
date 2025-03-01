3009692cad2917ce5f2b74c2e986b6729532055b
Dmytro Labusov <labusovdmytro@gmail.com>
2025-03-01 16:03:24 +0100

    finished

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,33 @@
-console.log('form');
+const key = 'feedback-form-state';
+
+const formData = { email: '', message: '' };
+
+const form = document.querySelector('.feedback-form');
+
+const savedData = JSON.parse(localStorage.getItem(key));
+if (savedData) {
+  Object.assign(formData, savedData);
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(key, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log('Send Data:', formData);
+
+  localStorage.removeItem(key);
+  form.reset();
+  formData.email = '';
+  formData.message = '';
+});
